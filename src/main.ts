fef03731ef53a9e7b4a8a896173ed0511d640d76
Flavia Kosti <flaviakosti@gmail.com>
2025-05-03 22:02:12 -0400

    Ngrx Setup Reducer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
+import { counterReducer } from './app/store/counter.reducer';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [provideStore({
+        /**Here are added all the reducers of the app */
+        counter: counterReducer
+    })]
+});
