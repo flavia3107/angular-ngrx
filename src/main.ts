06628e1c7b9d3efc4c6e80683a84eb48a996a988
Flavia Kosti <flaviakosti@gmail.com>
2025-05-05 17:40:14 -0400

    NgRx - Effects

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore({
+    providers: [
         /**Here are added all the reducers of the app */
-        counter: counterReducer
-    })]
+        provideStore({ counter: counterReducer }),
+        provideEffects()]
 });
