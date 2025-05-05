600c18a2512133882ebe098259e7f4433f68d4c3
Flavia Kosti <flaviakosti@gmail.com>
2025-05-05 16:16:58 -0400

    NgRX - Create actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,4 +1,8 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.action";
 
 const initialState = 0;
-export const counterReducer = createReducer(initialState);
+
+export const counterReducer = createReducer(initialState,
+	on(increment, (state) => state + 1)
+);
