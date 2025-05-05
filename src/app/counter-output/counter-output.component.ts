6370ee33be96786ecdf5a913b24c053efd370bb3
Flavia Kosti <flaviakosti@gmail.com>
2025-05-05 17:35:24 -0400

    NgRx - Selectors

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -2,6 +2,7 @@ import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -12,5 +13,7 @@ import { Observable } from 'rxjs';
 })
 export class CounterOutputComponent {
   private _store: Store<{ counter: number }> = inject(Store);
-  count$: Observable<number> = this._store.select('counter');
+  count$: Observable<number> = this._store.select(selectCount);
+  doubleCount$: Observable<number> = this._store.select(selectDoubleCount);
+
 }
