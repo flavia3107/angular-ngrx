fa9a10e2854c9206e70a203ac435d42447a0ef9b
Flavia Kosti <flaviakosti@gmail.com>
2025-05-06 19:36:53 -0400

    NgRx - Use store data in effects

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,16 +1,21 @@
-import { inject } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { tap } from "rxjs";
+import { Store } from "@ngrx/store";
+import { tap, withLatestFrom } from "rxjs";
 import { decrement, increment } from "./counter.action";
+import { selectCount } from "./counter.selectors";
 
+@Injectable()
 export class CounterEffects {
+	private _actions$: Actions = inject(Actions);
+	private _store: Store<{ counter: number }> = inject(Store);
+
 	saveCount = createEffect(() => this._actions$.pipe(
 		ofType(increment, decrement),
-		tap((action) => {
-			console.log('Ac', action);
+		withLatestFrom(this._store.select(selectCount)),
+		tap(([action, counter]) => {
+			console.log('Action', action, counter);
 			//or local storage
 		})
 	), { dispatch: false });
-	private _actions$: Actions = inject(Actions);
-
 }
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
 import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './app/store/counter.effects';
 
 bootstrapApplication(AppComponent, {
     providers: [
         /**Here are added all the reducers of the app */
         provideStore({ counter: counterReducer }),
-        provideEffects()]
+        provideEffects([CounterEffects])]
 });
